4171f990b085926387f62d461997fd73fdcda41d
Chelsea Harding - USU <122066671+chelseaharding@users.noreply.github.com>
2025-11-24 17:19:25 +0000

    updating a few more things

--- stock_market_trading/main.py
+++ stock_market_trading/main.py
@@ -59,7 +59,6 @@ def append_data(ticker):
     with open("/workspaces/data_3500_in_class_code_FA_2025/stock_market_trading/"+ticker+".csv", "a") as file:
         file.writelines(new_lines)
 
-
 def mean_reversion(prices):
     buy             = 0
     first_buy       = 0
@@ -93,7 +92,36 @@ def mean_reversion(prices):
     # print("% Return: \t", (total_profit/first_buy) * 100)
 
 def simple_moving_average(prices):
-    pass
+    buy             = 0
+    first_buy       = 0
+    total_profit    = 0
+    i               = 0
+
+    for price in prices:
+        if i > 4:
+            current_price = price
+            average_price = (prices[i-1] + prices[i-2] + prices[i-3] + prices[i-4] + prices[i-5])/5
+
+            if current_price > average_price and buy == 0:
+                buy = current_price
+                # print("Buying at: \t", buy)
+                if first_buy == 0:
+                    first_buy = current_price
+            elif current_price < average_price and buy != 0:
+                trade_profit = current_price - buy
+                buy = 0
+                # print("Selling at: \t", current_price)
+                # print("Trade Profit: \t", trade_profit)
+                total_profit += trade_profit
+
+        i += 1
+
+    return total_profit, (total_profit/first_buy)
+
+    # print("------------------------------------")
+    # print("Total Profit: \t", total_profit)
+    # print("First Buy: \t", first_buy)
+    # print("% Return: \t", (total_profit/first_buy) * 100)
 
 def load_prices(file):
     prices = [float(line.split(", ")[1]) for line in file.readlines()]
@@ -104,13 +132,17 @@ def save_results(results):
 
 for ticker in tickers:
     # inital_data_pull(ticker)
-    append_data(ticker)
+    # append_data(ticker)
     file = open("/workspaces/data_3500_in_class_code_FA_2025/stock_market_trading/"+ticker+".csv")
     prices = load_prices(file)
     results[ticker+"_Prices"] = prices
 
-    total_profit, returns = mean_reversion(prices)
-    results[ticker+"_MR_Profit"]  = total_profit
-    results[ticker+"_MR_Returns"] = returns
+    mr_total_profit, mr_returns = mean_reversion(prices)
+    results[ticker+"_MR_Profit"]  = mr_total_profit
+    results[ticker+"_MR_Returns"] = mr_returns
+
+    sma_total_profit, sma_returns = simple_moving_average(prices)
+    results[ticker+"_SMA_Profit"]  = sma_total_profit
+    results[ticker+"_SMA_Returns"] = sma_returns
 
 save_results(results)
